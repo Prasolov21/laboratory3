0d8494927300ea6e9687eb5480d0c35787bbc7fc
Prasolov21 <99333456+Prasolov21@users.noreply.github.com>
2022-03-16 22:07:56 +0300

    Update qwerty1.py

--- qwerty1.py
+++ qwerty1.py
@@ -1,14 +1,14 @@
 import math
-xc=1
-xf=0.9
+x1=1
+x2=0.9
 e=0.0000217
 r=100
 while r>e:
-    xp=xc
-    xc=xf
-    y=(2*xc)-math.log(xc + 2)
-    d=(2* xp)-math.log(xp + 2)
-    xf=xc-(((xc-xp)*y)/(y-d))
-    g=(2*xf)-math.log(xf+2)
+    x0=x1
+    x1=x2
+    y=(2*x1)-math.log(x1 + 2)
+    d=(2* x0)-math.log(x0 + 2)
+    x2=x1-(((x1-x0)*y)/(y-d))
+    g=(2*x2)-math.log(x2+2)
     r=abs(y-g)
     print(xf)
